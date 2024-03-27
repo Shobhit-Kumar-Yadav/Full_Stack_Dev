20aede823438490ed568692a275196d148af857d
Shobhit-Kumar-Yadav <143309281+Shobhit-Kumar-Yadav@users.noreply.github.com>
2024-03-27 10:05:17 +0530

    final commit

--- Registration-login/server/routes/user.js
+++ Registration-login/server/routes/user.js
@@ -47,27 +47,28 @@ router.post('/forgot-password', async (req, res) => {
         if(!user) {
             return res.json({message: "user not registered"})
         }
+        const token = jwt.sign({id: user._id}, process.env.KEY, {expiresIn: '5m'})
 
         var transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
-              user: 'youremail@gmail.com',
-              pass: 'yourpassword'
+              user: 'shobhitkumar9934@gmail.com',
+              pass: 'Shobhit'
             }
           });
           
           var mailOptions = {
-            from: 'youremail@gmail.com',
-            to: 'myfriend@yahoo.com',
-            subject: 'Sending Email using Node.js',
-            text: 'That was easy!'
+            from: 'shobhitkumar9934@gmail.com',
+            to: email,
+            subject: 'Reset Password',
+            text: `http://localhost:5173/resetPassword/${token}`
           };
           
           transporter.sendMail(mailOptions, function(error, info){
             if (error) {
-              console.log(error);
+              return res.json({message: "error sending email"})
             } else {
-              console.log('Email sent: ' + info.response);
+              return res.json({status:true, message: "email sent"})
             }
           });
 
